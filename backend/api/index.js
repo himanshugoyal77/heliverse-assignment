a9824332f082e4300c9af7b049234c66cce8bb95
himanshu goyal <hiteshgoyal943gmail.com>
2023-12-03 22:18:48 +0530

    add allow cors 2

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://heliverse-frontend-vite.vercel.app/",
+    origin: "https://heliverse-frontend-vite.vercel.app",
     credentials: true,
   })
 );
